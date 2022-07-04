3a4ee399260c508928419564f80c5c31b9201886
marinaparram <mapamarina@gmail.com>
2022-07-04 14:07:55 +0200

    cosas

--- src/main/java/com/example/NuevoProyecto/Security/SecurityConfiguration.java
+++ src/main/java/com/example/NuevoProyecto/Security/SecurityConfiguration.java
@@ -17,8 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.SecureRandom;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     RepositoryUserDetailsService userDetailsService;
