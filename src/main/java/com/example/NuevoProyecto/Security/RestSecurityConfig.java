8fe745f60bfb1825cb067749f4aeda8365ca6655
NicoleReyesRiv <100412750+NicoleReyesRiv@users.noreply.github.com>
2022-07-04 17:27:22 +0200

    rest

--- src/main/java/com/example/NuevoProyecto/Security/RestSecurityConfig.java
+++ src/main/java/com/example/NuevoProyecto/Security/RestSecurityConfig.java
@@ -27,7 +27,7 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http)throws Exception{
-        //http.antMatcher("/api/**");
+        http.antMatcher("/api/**");
         // Private endpoints
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/user/").hasRole("ADMIN");
